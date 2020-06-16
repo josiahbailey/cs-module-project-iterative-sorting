81df398fcd0ecd0bf967c7ff688da24b37a630ba
Josiah <reikiryo@yahoo.com>
2020-06-16 16:46:43 -0400

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,20 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for x in range(0, len(arr)):
+        if arr[x] == target:
+            return x
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        middle = int((low + high) / 2)
+        if target < arr[middle]:
+            high = middle - 1
+        elif target > arr[middle]:
+            low = middle + 1
+        else:
+            return middle
     return -1  # not found
